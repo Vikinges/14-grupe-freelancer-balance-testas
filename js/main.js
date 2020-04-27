07ae1e16f0d2e1fb4fec55d3c6da075a37e83bd1
Viking <37813004+Vikinges@users.noreply.github.com>
2020-04-27 10:45:57 +0300

    MonthArryADD

---
+++ js/main.js
@@ -0,0 +1,18 @@
+import account from './data.js';
+
+let monthsh =['January', 
+            'February',
+            'March' ,
+            'April',
+            'May' ,
+            'June' ,
+            'July' ,
+            'August', 
+            'September', 
+            'October' ,
+            "November" ,
+            'December' ]
+
+console.log(monthsh);
+console.log(account);
+
