8660f0110fc2a1b67d5d60eeabf0b847f376493b
Viking <37813004+Vikinges@users.noreply.github.com>
2020-04-27 11:34:25 +0300

    ClassMonth

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 import account from './data.js';
 
+class Month {
+    constructor(Month, income, expensive){
+    this.month=Month;
+    this.income=income;
+    this.expense=expensive;
+    this.init();
+
+    }
+   init(){
+  
+
+   }
+
+
+}
+
 let monthsh =['January', 
             'February',
             'March' ,
@@ -13,6 +29,103 @@ let monthsh =['January',
             "November" ,
             'December' ]
 
-console.log(monthsh);
+
+
+const DOM = document.querySelector('.table');
+console.log(DOM);
+let rows=1;
+ let HTML = `<div class="table-head">
+ <div class="cell">#</div>
+ <div class="cell">Mėnuo</div>
+ <div class="cell">Įplaukos</div>
+ <div class="cell">Išlaidos</div>
+ <div class="cell">Balansas</div>
+</div>
+<div class="table-content">
+<div class="table-row">
+<div class="cell">${rows++}</div>
+<div class="cell">${monthsh[rows-2]}</div>
+<div class="cell">${account[1].income} Eur</div>
+<div class="cell">${account[1].expense}</div>
+<div class="cell">${account[1].income - account[1].expense } Eur</div>
+</div>
+<div class="table-row">
+<div class="cell">${rows++}</div>
+<div class="cell">${monthsh[rows-2]}</div>
+<div class="cell">${account[4].income} Eur</div>
+<div class="cell">${account[4].expense}</div>
+<div class="cell">${(account[1].income - account[1].expense) + (account[4].income - account[4].expense)} Eur</div>
+</div><div class="table-row">
+<div class="cell">${rows++}</div>
+<div class="cell">${monthsh[rows-2]}</div>
+<div class="cell">${account[1].income} Eur</div>
+<div class="cell">${account[1].expense}</div>
+<div class="cell"> Eur</div>
+</div><div class="table-row">
+<div class="cell">${rows++}</div>
+<div class="cell">${monthsh[rows-2]}</div>
+<div class="cell">${account[1].income} Eur</div>
+<div class="cell">${account[1].expense}</div>
+<div class="cell">${account[1].income - account[1].expense } Eur</div>
+</div><div class="table-row">
+<div class="cell">${rows++}</div>
+<div class="cell">${monthsh[rows-2]}</div>
+<div class="cell">${account[1].income} Eur</div>
+<div class="cell">${account[1].expense}</div>
+<div class="cell">${account[1].income - account[1].expense } Eur</div>
+</div><div class="table-row">
+<div class="cell">${rows++}</div>
+<div class="cell">${monthsh[rows-2]}</div>
+<div class="cell">${account[1].income} Eur</div>
+<div class="cell">${account[1].expense}</div>
+<div class="cell">${account[1].income - account[1].expense } Eur</div>
+</div><div class="table-row">
+<div class="cell">${rows++}</div>
+<div class="cell">${monthsh[rows-2]}</div>
+<div class="cell">${account[1].income} Eur</div>
+<div class="cell">${account[1].expense}</div>
+<div class="cell">${account[1].income - account[1].expense } Eur</div>
+</div><div class="table-row">
+<div class="cell">${rows++}</div>
+<div class="cell">${monthsh[rows-2]}</div>
+<div class="cell">${account[1].income} Eur</div>
+<div class="cell">${account[1].expense}</div>
+<div class="cell">${account[1].income - account[1].expense } Eur</div>
+</div><div class="table-row">
+<div class="cell">${rows++}</div>
+<div class="cell">${monthsh[rows-2]}</div>
+<div class="cell">${account[1].income} Eur</div>
+<div class="cell">${account[1].expense}</div>
+<div class="cell">${account[1].income - account[1].expense } Eur</div>
+</div><div class="table-row">
+<div class="cell">${rows++}</div>
+<div class="cell">${monthsh[rows-2]}</div>
+<div class="cell">${account[1].income} Eur</div>
+<div class="cell">${account[1].expense}</div>
+<div class="cell">${account[1].income - account[1].expense } Eur</div>
+</div><div class="table-row">
+<div class="cell">${rows++}</div>
+<div class="cell">${monthsh[rows-2]}</div>
+<div class="cell">${account[1].income} Eur</div>
+<div class="cell">${account[1].expense}</div>
+<div class="cell">${account[1].income - account[1].expense } Eur</div>
+</div><div class="table-row">
+<div class="cell">${rows++}</div>
+<div class="cell">${monthsh[rows-2]}</div>
+<div class="cell">${account[1].income} Eur</div>
+<div class="cell">${account[1].expense}</div>
+<div class="cell">${account[1].income - account[1].expense } Eur</div>
+</div>
+</div>
+<div class="table-footer">
+                    <div class="cell"></div>
+                    <div class="cell"></div>
+                    <div class="cell">0.00 Eur</div>
+                    <div class="cell">0.00 Eur</div>
+                    <div class="cell">0.00 Eur</div>
+                </div>`    
+DOM.innerHTML = HTML;       
+// console.log(monthsh);
 console.log(account);
 
+// function ()
